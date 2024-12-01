00162bce67278a217bf3ae54e1716cc4bfdf41ad
Jo Seung Hyo <seunghyo7742@naver.com>
2024-12-01 19:08:24 +0900

    Setting: 절대경로 셋팅

--- nanumJang/src/components/Layouts/BaseLayout.tsx
+++ nanumJang/src/components/Layouts/BaseLayout.tsx
@@ -2,17 +2,17 @@ import ModalProvider from '@/providers/modalProvider';
 
 const BaseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto flex h-svh w-full justify-center bg-content1">
+    <div className="flex justify-center w-full mx-auto h-svh bg-content1">
       <aside className="hidden p-6 text-gray-700 lg:flex lg:items-center">
         <div className="max-w-96">
-          <img src="/images/landing.png" alt="Lading Image" />
+          <div>image</div>
         </div>
       </aside>
       {/* 우측 앱화면 */}
       <main className="relative h-svh w-full max-w-[500px] bg-[#F4F4F5] shadow-xl lg:min-w-[500px]">
         <div
           id="scroll-parent"
-          className="h-svh max-h-svh w-full overflow-y-scroll scrollbar-hide"
+          className="w-full overflow-y-scroll h-svh max-h-svh scrollbar-hide"
         >
           {children}
         </div>
