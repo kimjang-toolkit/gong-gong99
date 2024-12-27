119cc1ff2311612343b21b093b4862961572362a
Jo Seung Hyo <seunghyo7742@naver.com>
2024-12-28 01:58:00 +0900

    Feat: 전체적으로 적용되는 레이아웃 컴포넌트화

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -2,9 +2,9 @@ function DefaultLayout({ children }: { children: React.ReactNode[] }) {
   const [header, content, footer] = children;
   return (
     <>
-      <header className="fixed top-0 w-full">{header}</header>
-      <main className="px-5 py-6">{content}</main>
-      <footer className="fixed bottom-0 w-full">{footer}</footer>
+      <header className="absolute top-0 w-full ">{header}</header>
+      <main className="px-5 mt-[64px]">{content}</main>
+      <footer className="absolute bottom-0 w-full">{footer}</footer>
     </>
   );
 }
