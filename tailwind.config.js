89f1a68acaa7b517bfa6790cddafd0e98061c212
Jo Seung Hyo <seunghyo7742@naver.com>
2024-12-30 23:42:32 +0900

    Setting: nextui 삭제

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const {nextui} = require("@nextui-org/react");
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
@@ -44,8 +41,7 @@ export default {
   },
   darkMode: 'class',
   plugins: [
-    nextui()
-    ,
+    nextui(),
     ({ addUtilities }) => {
       addUtilities({
         '.default-box': {
