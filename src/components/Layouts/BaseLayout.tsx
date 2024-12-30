f90a001b738b04c58f7024d72e61d6b5238e7cfc
Jo Seung Hyo <seunghyo7742@naver.com>
2024-12-30 21:21:14 +0900

    Style: 배경색상 변경

--- src/components/Layouts/BaseLayout.tsx
+++ src/components/Layouts/BaseLayout.tsx
@@ -2,14 +2,14 @@ import ModalProvider from '@/providers/modalProvider';
 
 const BaseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center w-full mx-auto h-svh bg-content1">
+    <div className="flex justify-center w-full mx-auto h-svh bg-[#F4F4F5]">
       <aside className="hidden p-6 text-gray-700 lg:flex lg:items-center">
         <div className="max-w-96">
           <div>image</div>
         </div>
       </aside>
       {/* 우측 앱화면 */}
-      <main className="relative h-svh w-full max-w-[500px] bg-[#F4F4F5] shadow-xl lg:min-w-[500px]">
+      <main className="relative h-svh w-full max-w-[500px] bg-[#FFFFF] shadow-xl lg:min-w-[500px]">
         {children}
         <ModalProvider />
       </main>
