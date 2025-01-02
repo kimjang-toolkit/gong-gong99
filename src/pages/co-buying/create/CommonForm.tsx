7ce2d9d43d388a05ae173b2ef209b30d2a8eaf6b
seunghyo <seunghyo7742@naver.com>
2025-01-02 17:02:21 +0900

    Modify: 인풋 별로  id추가

--- src/pages/co-buying/create/CommonForm.tsx
+++ src/pages/co-buying/create/CommonForm.tsx
@@ -23,6 +23,7 @@ function CommonForm() {
   return (
     <>
       <BoarderedInput
+        id="productName"
         label="상품 이름"
         placeholder="상품 이름을 입력 (5~100자)"
         regex={/^[a-zA-Z0-9가-힣\s]{5,100}$/}
@@ -30,6 +31,7 @@ function CommonForm() {
         regexErrorMessage="상품 이름은 5~100자의 한글, 영문, 숫자만 입력 가능합니다"
       />
       <BoarderedInput
+        id="totalPrice"
         label="상품 총액"
         placeholder="상품 총 가격 입력"
         type="number"
@@ -37,12 +39,14 @@ function CommonForm() {
         defaultValue={0}
       />
       <BoarderedInput
+        id="productLink"
         label="상품 링크 (선택)"
         placeholder="상품 정보관련 링크 입력"
         type="url"
       />
       {/* 추후 모바일용 날짜 인풋 컴포넌트 만들기 */}
       <BoarderedInput
+        id="deadline"
         label="신청 마감일"
         type="date"
         // leftIcon={
@@ -54,7 +58,7 @@ function CommonForm() {
       />
       <section>
         <p className="pl-0.5 mb-1 text-caption text-default-600">
-          공구나눔 방식{' '}
+          공구나눔 방식
         </p>
         <div className="flex gap-4">
           <button
