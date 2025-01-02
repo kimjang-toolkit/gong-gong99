02008934d0eafe3cc2a058fadc70ded7325a9f6e
Jo Seung Hyo <seunghyo7742@naver.com>
2025-01-03 00:17:59 +0900

    Design: 바텀버튼 css수정

--- src/components/Button/BottomButton.tsx
+++ src/components/Button/BottomButton.tsx
@@ -6,7 +6,7 @@ interface BottomButtonProps {
 function BottomButton({ label, onClick }: BottomButtonProps) {
   return (
     <button
-      className="w-full py-5 text-center text-h2 text-default-50 bg-primary-300"
+      className="w-full py-4 text-center text-h3-bold text-default-50 bg-primary-300"
       onClick={onClick}
     >
       {label}
