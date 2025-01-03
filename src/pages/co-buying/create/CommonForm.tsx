c903ca7ad013263302b30f28a2a6ff6bfecbc132
seunghyo <seunghyo7742@naver.com>
2025-01-03 16:13:39 +0900

    Modify: FormSection CreatePage로 이동

--- src/pages/co-buying/create/CommonForm.tsx
+++ src/pages/co-buying/create/CommonForm.tsx
@@ -1,27 +1,6 @@
 import BoarderedInput from '@/components/Input/BoarderedInput';
-import { cn } from '@/lib/utils';
-import DevideByAttendeeForm from '@/pages/co-buying/create/DevideByAttendeeForm';
-import DevideByQuantityForm from '@/pages/co-buying/create/DevideByQuantityForm';
-import { useState } from 'react';
 
 function CommonForm() {
-  const [devideType, setDevideType] = useState<'quantity' | 'person'>(
-    'quantity'
-  );
-
-  // 공구나눔방식 선택 클래스
-  const selectedTypeClass = 'bg-primary-300 text-white';
-  const unselectedTypeClass = 'bg-default-300 text-default-600';
-
-  const devideTypeDescription = {
-    quantity: '참여자들이 필요한 수량만큼 선택할 수 있도록 해요.',
-    person: '참여인원 수대로 금액 및 상품수량을 균등하게 나눠요.',
-  };
-
-  const handleDevideTypeChange = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setDevideType(e.currentTarget.id as 'quantity' | 'person');
-  };
   return (
     <>
       <BoarderedInput
@@ -58,44 +37,6 @@ function CommonForm() {
         // }
         min={new Date().toISOString().split('T')[0]}
       />
-      <section>
-        <p className="pl-0.5 mb-1 text-caption text-default-600">
-          공구나눔 방식
-        </p>
-        <div className="flex gap-4">
-          <button
-            id="quantity"
-            className={cn(
-              'text-center text-body rounded-lg h-[62px] flex-1 shadow-sm',
-              devideType === 'quantity'
-                ? selectedTypeClass
-                : unselectedTypeClass
-            )}
-            onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
-              handleDevideTypeChange(e)
-            }
-          >
-            수량으로 나누기
-          </button>
-          <button
-            id="person"
-            className={cn(
-              'text-center  text-body rounded-lg h-[62px] flex-1 shadow-sm',
-              devideType === 'person' ? selectedTypeClass : unselectedTypeClass
-            )}
-            onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
-              handleDevideTypeChange(e)
-            }
-          >
-            인원으로 나누기
-          </button>
-        </div>
-        <p className="mt-1 text-tiny text-default-500">
-          * {devideTypeDescription[devideType]}
-        </p>
-      </section>
-      {devideType === 'quantity' && <DevideByQuantityForm />}
-      {devideType === 'person' && <DevideByAttendeeForm />}
     </>
   );
 }
--- src/pages/co-buying/create/index.tsx
+++ src/pages/co-buying/create/index.tsx
@@ -1,13 +1,47 @@
 import BottomButton from '@/components/Button/BottomButton';
 import TitleHeader from '@/components/Header/TitleHeader';
 import DefaultLayout from '@/components/Layouts/DefaultLayout';
-import FormSection from '@/pages/co-buying/create/FormSection';
+import CommonForm from '@/pages/co-buying/create/CommonForm';
+import DevideByQuantityForm from '@/pages/co-buying/create/DevideByQuantityForm';
+import DevideByAttendeeForm from '@/pages/co-buying/create/DevideByAttendeeForm';
+import DevideTypeSection from '@/pages/co-buying/create/DevideTypeSection';
+import { createContext, useMemo, useState } from 'react';
+
+const FormContext = createContext({});
+export { FormContext };
 
 function CreatePage() {
+  const [devideType, setDevideType] = useState<'quantity' | 'person'>(
+    'quantity'
+  );
+  const [formData, setFormData] = useState({});
+
+  const formContextValue = useMemo(
+    () => ({
+      devideType,
+      setDevideType,
+      formData,
+      setFormData,
+    }),
+    [devideType, formData]
+  );
+
   return (
     <DefaultLayout>
       <TitleHeader title="임시 메인" />
-      <FormSection />
+      <FormContext.Provider value={formContextValue}>
+        <form className="flex flex-col h-full gap-4">
+          {/* 1.상품 기본정보 폼 */}
+          <CommonForm />
+
+          {/* 2. 공구 나눔방식 선택 */}
+          <DevideTypeSection />
+
+          {/* 3. 공구 나눔방식 선택에 따라 수량으로 나누기 폼/ 인원으로 나누기 폼 */}
+          {devideType === 'quantity' && <DevideByQuantityForm />}
+          {devideType === 'person' && <DevideByAttendeeForm />}
+        </form>
+      </FormContext.Provider>
       <BottomButton label="다음" onClick={() => {}} />
     </DefaultLayout>
   );
