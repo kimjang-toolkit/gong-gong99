2ce9dea6aaf0d9c2c47032038c6e5a83c0aa1465
seunghyo <seunghyo7742@naver.com>
2025-01-03 16:41:23 +0900

    Fix: 소나큐브 에러

--- src/components/Button/BottomButton.tsx
+++ src/components/Button/BottomButton.tsx
@@ -3,11 +3,16 @@ interface BottomButtonProps {
   onClick: () => void;
 }
 
-function BottomButton({ label, onClick }: BottomButtonProps) {
+function BottomButton({
+  label,
+  onClick,
+  ...props
+}: Readonly<BottomButtonProps>) {
   return (
     <button
       className="w-full py-4 text-center text-h3-bold text-default-50 bg-primary-300"
       onClick={onClick}
+      {...props}
     >
       {label}
     </button>
