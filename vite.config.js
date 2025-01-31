25a6227ad5edceb0f79d14615faf1868af7d915a
seunghyo <seunghyo7742@naver.com>
2025-01-31 16:26:02 +0900

    Setting: 배포...

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
+import svgr from 'vite-plugin-svgr';
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(), svgr()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@domain': fileURLToPath(new URL('./common-type/src/domain', import.meta.url)),
+            '@interface': fileURLToPath(new URL('./common-type/src/interface', import.meta.url)),
+        },
+    },
+});
