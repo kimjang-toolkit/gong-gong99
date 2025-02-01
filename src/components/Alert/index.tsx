018233b76be9cac870ce5522d753a29593f2d61c
joseunghyo7742 <seunghyo7742@naver.com>
2025-02-01 19:37:34 +0900

    Feat: Alert 전역상태 및 프로파이더 작성

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,26 +1,27 @@
 import SuccessIcon from '@/assets/icons/alert/success.svg?react';
 import FailIcon from '@/assets/icons/alert/fail.svg?react';
 import { useEffect } from 'react';
+import useAlertStore from '@/stores/alertStore';
 
 interface AlertProps {
   status: 'success' | 'fail';
   label: string;
-  setIsOpen: (isOpen: boolean) => void;
 }
 const statusIcon = {
   success: <SuccessIcon />,
   fail: <FailIcon />,
 };
 
-export default function Alert({ status, label, setIsOpen }: AlertProps) {
+export default function Alert({ status, label }: AlertProps) {
+  const showAlert = useAlertStore((state) => state.showAlert);
   useEffect(() => {
     setTimeout(() => {
-      setIsOpen(false);
+      showAlert(null);
     }, 2000);
   }, []);
   return (
     <div
-      className="flex items-center bg-[#3F3F46] rounded-2xl px-3 py-2 whitespace-nowrap text-white text-tiny-bold gap-2 absolute -translate-x-1/2 left-1/2 z-[50] top-16 shadow-sm
+      className="flex items-center bg-[#3F3F46] rounded-2xl px-3 py-2 whitespace-nowrap text-white text-tiny-bold gap-2 relative -translate-x-1/2 left-1/2 z-[50] top-16 shadow-sm
     animate-slideIn animate-duration-200 "
     >
       {statusIcon[status]}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { AlertProvider } from './providers/alertProvider.tsx';
 const queryClient = new QueryClient({});
 
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AlertProvider>
+        <App />
+      </AlertProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
