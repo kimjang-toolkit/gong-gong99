3629d5984034d5f64e087612c52e84f096d7b5e0
joseunghyo7742 <seunghyo7742@naver.com>
2025-02-11 00:14:56 +0900

    Setting: 서브모듈 버전 변경

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="fixed bottom-0 left-0 bg-default-100 h-[56px] flex items-center justify-end px-4 w-full py-1">
-      <button className="flex flex-col items-center justify-center text-caption">
+      <button className="flex flex-col items-center justify-center typo-caption">
         <div className="w-4 h-4 bg-black"></div>
         <p>최근 본 글</p>
       </button>
