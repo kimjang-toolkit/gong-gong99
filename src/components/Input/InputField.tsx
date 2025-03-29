3e4649801aefa7ed192793944bb9e2e534550926
seunghyo <seunghyo7742@naver.com>
2025-03-30 01:58:01 +0900

    Feat: 상품총액 숫자형식 세자리마다 "," 추가

--- src/components/Input/InputField.tsx
+++ src/components/Input/InputField.tsx
@@ -1,12 +1,36 @@
-import { useInputContext } from '@/components/Input/context';
-import { cn } from '@/lib/utils';
-import { InputHTMLAttributes } from 'react';
+import { useInputContext } from "@/components/Input/context";
+import { cn } from "@/lib/utils";
+import { InputHTMLAttributes } from "react";
 
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
 }
 export default function InputField({ className, ...props }: InputFieldProps) {
   const { value, setValue } = useInputContext();
+  const valueType = typeof value;
+
+  // console.log("value1 : ", value);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const nextValue = e.target.value;
+    if (valueType === "number") {
+      // const numericValue = nextValue.replace(",", "");
+      // console.log("numericValue : ", numericValue);
+      setValue(Number(nextValue));
+    } else {
+      setValue(nextValue);
+    }
+  };
+
+  // let displayValue = value;
+
+  // if (valueType === "number") {
+  //   displayValue = value.toLocaleString();
+  // }
+
+  // console.log("value2 : ", displayValue);
+
+  // const displayValue =
+  //   typeof value === "number" ? value.toLocaleString() : value;
 
   return (
     <input
@@ -14,9 +38,10 @@ export default function InputField({ className, ...props }: InputFieldProps) {
       value={value}
       autoComplete="off"
       spellCheck={false}
-      onChange={(e) => setValue(e.target.value)}
+      // onChange={(e) => setValue(e.target.value)}
+      onChange={handleChange}
       className={cn(
-        'text-body text-black w-full focus:outline-none  bg-transparent',
+        "text-body text-black w-full focus:outline-none  bg-transparent",
         className
       )}
     />
