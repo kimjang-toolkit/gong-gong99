8443ac67fb4e72217069a5bfd5b67dc1929c29d7
seunghyo <seunghyo7742@naver.com>
2025-03-30 23:03:25 +0900

    QA009: 상품 총액 타입 문제 해결

--- src/pages/co-buying/create/CreateForm.tsx
+++ src/pages/co-buying/create/CreateForm.tsx
@@ -41,7 +41,7 @@ export default function CreateForm({
       <Form.Input name="totalPrice">
         <Input.Label>상품 총액</Input.Label>
         {/* 숫자 입력 시 쉼표 추가를 위해 string 타입 사용 */}
-        <Input.Field placeholder="상품 총액을 입력해주세요." type="text" />
+        <Input.Field placeholder="상품 총액을 입력해주세요." type="string" />
         <Input.Suffix>원</Input.Suffix>
         <Input.Description />
       </Form.Input>
