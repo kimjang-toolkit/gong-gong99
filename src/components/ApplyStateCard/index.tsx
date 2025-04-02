5ae66db73c7c5b3538c819a413003b7da2320059
joseunghyo7742 <seunghyo7742@naver.com>
2025-04-02 22:17:05 +0900

    Feat: 나눔현황 컴포넌트 ui 완성

--- src/components/ApplyStateCard/index.tsx
+++ src/components/ApplyStateCard/index.tsx
@@ -20,7 +20,9 @@ export default function ApplyStateCard({
     0
   );
 
-  const handleCheckClick = () => {};
+  const handleCheckClick = () => {
+    
+  };
   return (
     <section
       className={cn(
@@ -36,9 +38,22 @@ export default function ApplyStateCard({
         </h3>
         <input
           type="checkbox"
-          checked={attendeeData.attendeeSharingCheckYN}
-          onChange={handleCheckClick}
-          className={`${showCheckbox ? 'block' : 'hidden'}`}
+          className={`relative appearance-none w-4 h-4 border-default-300 border-2 rounded-full
+              transition-all duration-300
+              checked:bg-default-300
+              checked:after:content-["✔"]
+              checked:after:text-white 
+              checked:after:text-[10px]
+              checked:after:absolute
+              checked:after:top-[-1px]
+              checked:after:left-[2px]
+              after:opacity-0
+              after:scale-0
+              checked:after:opacity-100
+              checked:after:scale-100
+              checked:after:transition-all
+              checked:after:duration-300
+              ${showCheckbox ? 'block' : 'hidden'}`}
         />
       </header>
       <div className="flex flex-col gap-2 mt-4">
