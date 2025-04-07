94107b375e7610d6d186e60267588a29ed1cbcb9
seunghyo <seunghyo7742@naver.com>
2025-04-08 00:17:35 +0900

    Modify: 비밀번호 검사 후 데이터 전달, 공유 체크 관련 인터페이스 속성 수정

--- src/api/mutations/useSharingCheck.ts
+++ src/api/mutations/useSharingCheck.ts
@@ -1,9 +1,9 @@
-import { cobuyingService } from '@/api/services/cobuying';
-import { useMutation } from '@tanstack/react-query';
+import { cobuyingService } from "@/api/services/cobuying";
+import { useMutation } from "@tanstack/react-query";
 
 export default function useSharingCheck(id: string, ownerName: string) {
   return useMutation({
-    mutationFn: (body: { isShared: boolean; attendeeName: string }) =>
+    mutationFn: (body: { isShared: boolean; name: string }) =>
       cobuyingService.putShareCheck(id, body, ownerName),
   });
 }
