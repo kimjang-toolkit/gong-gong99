64f07c948db2b585b704069de099ea39f53374be
seunghyo <seunghyo7742@naver.com>
2025-04-08 21:33:36 +0900

    Fix: 수량 나눔 시 옵션 신청 수량이 0개 일 때 부담액이 Nan으로 뜨는 문제 해결

--- src/pages/co-buying/[id]/applyList-section.tsx
+++ src/pages/co-buying/[id]/applyList-section.tsx
@@ -1,5 +1,13 @@
-import ApplyStateCard from '@/components/ApplyStateCard';
-import { CoBuyingDetail } from '@interface/cobuying';
+import ApplyStateCard from "@/components/ApplyStateCard";
+import { CoBuyingDetail, QuantityCoBuyingDetail } from "@interface/cobuying";
+import { DivideType } from "@domain/cobuying";
+
+// 타입 가드 함수
+function isQuantityCoBuying(
+  data: CoBuyingDetail
+): data is QuantityCoBuyingDetail {
+  return data.type === DivideType.quantity;
+}
 
 export default function ApplyListSection({
   data,
@@ -15,7 +23,12 @@ export default function ApplyListSection({
     <section className={className}>
       <p className="typo-caption text-default-500 mb-1.5">나눔 현황</p>
       {attendeeList?.map((attendee) => (
-        <ApplyStateCard attendeeData={attendee} showCheckbox={canEdit} />
+        <ApplyStateCard
+          key={attendee.name}
+          attendeeData={attendee}
+          showCheckbox={canEdit}
+          unitPrice={isQuantityCoBuying(data) ? data.unitPrice : undefined}
+        />
       ))}
     </section>
   );
