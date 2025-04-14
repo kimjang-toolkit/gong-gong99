7a348cc1640d0e1bf3e7eaa08bb631eb7d6c0608
seunghyo <seunghyo7742@naver.com>
2025-04-14 17:31:05 +0900

    Feat: 공구글 없을 때 fallback띄우기

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/lib/utils';
-
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+import { motion, HTMLMotionProps } from 'framer-motion';
+interface ButtonProps extends HTMLMotionProps<'button'> {
   label: string;
   size: 'small' | 'large';
   onClick?: () => void;
@@ -17,8 +17,9 @@ export default function Button({
   ...props
 }: ButtonProps) {
   return (
-    <button
+    <motion.button
       type={type}
+      whileTap={{ scale: 0.95 }}
       className={cn(
         'bg-primary-300 active:brightness-95',
         size === 'small'
@@ -31,6 +32,6 @@ export default function Button({
       {...props}
     >
       <p className="text-white">{label}</p>
-    </button>
+    </motion.button>
   );
 }
--- src/const/cobuyingStatus.ts
+++ src/const/cobuyingStatus.ts
@@ -1,14 +1,17 @@
 export const COBUYING_STATUS_MAP = {
   1: {
-    label: '신청중',
+    label: '모집중',
     color: 'primary',
+    fallback: '모집중인 공구글이 없습니다.',
   },
   2: {
     label: '나눔중',
     color: 'yellow',
+    fallback: '나눔중인 공구글이 없습니다.',
   },
   3: {
     label: '나눔완료',
     color: 'neutral',
+    fallback: '나눔완료인 공구글이 없습니다.',
   },
 } as const;
